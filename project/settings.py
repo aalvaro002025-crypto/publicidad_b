dd5bef5888d32dd7431adea49132fc92a74c98df
aalvaro002025-crypto <aalvaro002025@gmail.com>
2025-10-05 17:16:47 -0500

    ll

--- project/settings.py
+++ project/settings.py
@@ -133,7 +133,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
@@ -141,6 +141,19 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
     "https://publicidad-4apps.vercel.app",
     "https://publicidad-frontend.vercel.app",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 
 # REST Framework
