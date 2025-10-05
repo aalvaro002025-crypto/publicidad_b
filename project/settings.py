83b5ed252974cd44d99133abf2e572548dd40b9b
aalvaro002025-crypto <aalvaro002025@gmail.com>
2025-10-05 16:46:05 -0500

    ll

--- project/settings.py
+++ project/settings.py
@@ -139,6 +139,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "https://publicidad-4apps.vercel.app",
+    "https://publicidad-frontend.vercel.app",
 ]
 
 # REST Framework
