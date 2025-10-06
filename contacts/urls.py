469408e63ba5f047bbc0732646cb1f1e0ab0bd59
aalvaro002025-crypto <aalvaro002025@gmail.com>
2025-10-06 06:17:00 -0500

    ll

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('contact', views.create_contact, name='create_contact'),
-    path('contacts', views.list_contacts, name='list_contacts'),
-    path('health', views.health_check, name='health_check'),
+    path('submit/', views.create_contact, name='create_contact'),
+    path('contacts/', views.list_contacts, name='list_contacts'),
+    path('health/', views.health_check, name='health_check'),
 ]
